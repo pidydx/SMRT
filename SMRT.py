4a91a11e6a10fba9318f3245af0f1a0f89c32793
Sean Gillespie <seagill@yahoo-inc.com>
2014-07-02 13:17:28 -0700

    Added Text Transforms Char Swap and RotX

--- SMRT.py
+++ SMRT.py
@@ -2,12 +2,15 @@ import sublime, sublime_plugin
 import base64
 import hashlib
 import time
+import re
+from string import maketrans
 
 class BaseXxEncodeCommand(sublime_plugin.TextCommand):
     def run(self, edit, xx=64, table=None):
         for sel in self.view.sel():
             if not sel.empty():
                 text = self.view.substr(sel)
+                bxxtext = "*No Encoding Selected*"
                 if xx == 64:
                     bxxtext = base64.b64encode(text)
                 if xx == 32:
@@ -22,6 +25,7 @@ class BaseXxDecodeCommand(sublime_plugin.TextCommand):
             if not sel.empty():
             #TODO Regex for charset and check/correct padding if necessary
                 bxxtext = self.view.substr(sel)
+                text = "*No Decoding Selected*"
                 if xx == 64:
                     text = base64.b64decode(bxxtext)
                 if xx == 32:
@@ -29,6 +33,16 @@ class BaseXxDecodeCommand(sublime_plugin.TextCommand):
                 if xx == 16:
                     text = base64.b16decode(bxxtext)
                 self.view.replace(edit, sel, text)
+
+class TextTranslateCommand(sublime_plugin.TextCommand):
+    def run(self, edit, rot, transin="AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"):
+        transout = transin[rot:] + transin[:rot]
+        rottrans = maketrans(transin,transout)
+        for sel in self.view.sel():
+            if not sel.empty():
+                text = self.view.substr(sel)
+                rottext = str(text).translate(rottrans)
+                self.view.replace(edit, sel, rottext)
 
 class Rot13Command(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -38,7 +52,7 @@ class Rot13Command(sublime_plugin.TextCommand):
                 rot13text = text.encode('rot13')
                 self.view.replace(edit, sel, rot13text)
 
-class md5Command(sublime_plugin.TextCommand):
+class Md5Command(sublime_plugin.TextCommand):
     def run(self, edit):
         for sel in self.view.sel():
             if not sel.empty():
@@ -46,7 +60,7 @@ class md5Command(sublime_plugin.TextCommand):
                 hashed = hashlib.md5(hashtext).hexdigest()
                 self.view.replace(edit, sel, hashed)
 
-class sha1Command(sublime_plugin.TextCommand):
+class Sha1Command(sublime_plugin.TextCommand):
     def run(self, edit):
         for sel in self.view.sel():
             if not sel.empty():
@@ -59,8 +73,46 @@ class TimestampFromIntCommand(sublime_plugin.TextCommand):
         for sel in self.view.sel():
             if not sel.empty():
                 timeint = self.view.substr(sel)
+                timetext = "*No Timestamp Format Selected*"
                 if format == "Unix":
                     timetext = time.strftime("%d-%b-%Y %H:%M:%S", time.gmtime(int(timeint)))
                 self.view.replace(edit, sel, timetext)
 
+class DisplayInputErrorCommand(sublime_plugin.TextCommand):
+    def run(self, edit, errortext="*Unknown Error*"):
+        for sel in self.view.sel():
+            if not sel.empty():
+                self.view.replace(edit, sel, errortext)
+
+class GetTextRotValue(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.show_input_panel('Rotation', '', self.on_done, None, None)
+
+    def on_done(self, rot):
+        if re.search('^[0-9]+$', rot):
+            rot = int(rot)
+            if self.window.active_view():
+                self.window.active_view().run_command("text_translate", {"rot": rot*2})
+        else:
+            if self.window.active_view():
+                self.window.active_view().run_command("display_input_error", {"errortext": "*Non-integer Input*"})
+
+class GetSwapMap(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.show_input_panel('Swap Map', '', self.on_done, None, None)
+
+    def on_done(self, swapmap):
+        if re.search('[^:]+:[^:]+', swapmap):
+            seta, setb = swapmap.split(":")
+            if len(seta) == len(setb):
+                rot = len(seta)
+                transin = seta + setb
+                if self.window.active_view():
+                    self.window.active_view().run_command("text_translate", {"rot": rot, "transin": transin})
+            else:
+                if self.window.active_view():
+                    self.window.active_view().run_command("display_input_error", {"errortext": "*Invalid Swap Map: Use Xx:Yy format*"})
+        else:
+            if self.window.active_view():
+                self.window.active_view().run_command("display_input_error", {"errortext": "*Invalid Swap Map: Use Xx:Yy format*"})
 
