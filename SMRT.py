6d9286cc6c7f7bb8e0e05ebb84983fe8120dac14
Sean Gillespie <sean.gillespie.32@gmail.com>
2015-10-18 00:32:50 -0700

    Finished ST2 to ST3 conversion and expanded compression/decompression commands

--- SMRT.py
+++ SMRT.py
@@ -15,18 +15,18 @@
 
 import sublime
 import sublime_plugin
+import io
+import gzip
 import base64
 import hashlib
 import time
 import re
 import binascii
 import zlib
-import urllib
+import urllib.parse
 import socket
 import struct
 from SMRT.pescanner import pescanner
-
-# from string import maketrans
 
 
 def ParseHex(hextext):
@@ -77,6 +77,7 @@ class SwitchEndiannessCommand(sublime_plugin.TextCommand):
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
 
+
 class IntToIpCommand(sublime_plugin.TextCommand):
     def run(self, edit, order):
         for sel in self.view.sel():
@@ -91,64 +92,78 @@ class IntToIpCommand(sublime_plugin.TextCommand):
                 else:
                     self.view.replace(edit, sel, "*Non-integer Input*")
 
+
 class IpToIntCommand(sublime_plugin.TextCommand):
     def run(self, edit, order):
         for sel in self.view.sel():
             if not sel.empty():
                 iptext = self.view.substr(sel)
-                if re.search('^((([01]{0,1}[0-9]{1,2})|(2[0-5]{2}))\.){3}(([01]{0,1}[0-9]{1,2})|(2[0-5]{2}))$',iptext):
+                if re.search('^((([01]{0,1}[0-9]{1,2})|(2[0-5]{2}))\.){3}(([01]{0,1}[0-9]{1,2})|(2[0-5]{2}))$', iptext):
                     if order == "N":
-                        ipint =  struct.unpack(">L", socket.inet_aton(iptext))[0]
+                        ipint = struct.unpack(">L", socket.inet_aton(iptext))[0]
                     if order == "H":
-                        ipint =  struct.unpack("<L", socket.inet_aton(iptext))[0]
+                        ipint = struct.unpack("<L", socket.inet_aton(iptext))[0]
                     inttext = str(ipint)
                     self.view.replace(edit, sel, inttext)
                 else:
                     self.view.replace(edit, sel, "*Non-IPv4 Input*")
+
 
 class UrlUnquoteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for sel in self.view.sel():
             if not sel.empty():
                 urltext = self.view.substr(sel)
-                urltext = urllib.unquote(urltext)
+                urltext = urllib.parse.unquote(urltext)
                 self.view.replace(edit, sel, urltext)
+
 
 class UrlQuoteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for sel in self.view.sel():
             if not sel.empty():
                 urltext = self.view.substr(sel)
-                urltext = urllib.quote(urltext)
+                urltext = urllib.parse.quote(urltext)
                 self.view.replace(edit, sel, urltext)
 
-class ZlibCompressCommand(sublime_plugin.TextCommand):
-    def run (self, edit):
+
+class CompressCommand(sublime_plugin.TextCommand):
+    def run(self, edit, cformat='zlib'):
         for sel in self.view.sel():
             if not sel.empty():
                 hextext = ParseHex(self.view.substr(sel))
-                if hextext != None:
+                if hextext is not None:
                     ddata = binascii.unhexlify(hextext)
-                    cdata = zlib.compress(ddata)[2:-4]
+                    if cformat == 'zlib':
+                        cdata = zlib.compress(ddata)
+                    if cformat == 'deflate':
+                        cdata = zlib.compress(ddata)[2:-4]
+                    if cformat == 'gzip':
+                        gzip_out = io.BytesIO()
+                        with gzip.GzipFile(fileobj=gzip_out, mode='w') as fd:
+                            fd.write(ddata)
+                        cdata = gzip_out.getvalue()
                     hextext = binascii.hexlify(cdata)
-                    formathex = FormatHex(hextext)
+                    formathex = FormatHex(hextext.decode('utf-8'))
                     self.view.replace(edit, sel, formathex)
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
 
+
 class ZlibDecompressCommand(sublime_plugin.TextCommand):
-    def run (self, edit):
+    def run(self, edit, wbits=15):
         for sel in self.view.sel():
             if not sel.empty():
                 hextext = ParseHex(self.view.substr(sel))
-                if hextext != None:
+                if hextext is not None:
                     cdata = binascii.unhexlify(hextext)
-                    ddata = zlib.decompress(cdata,-15)
+                    ddata = zlib.decompress(cdata, wbits)
                     hextext = binascii.hexlify(ddata)
-                    formathex = FormatHex(hextext)
+                    formathex = FormatHex(hextext.decode('utf-8'))
                     self.view.replace(edit, sel, formathex)
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
+
 
 class HexEncodeCommand(sublime_plugin.TextCommand):
     def run(self, edit, encoding="ascii"):
@@ -156,59 +171,64 @@ class HexEncodeCommand(sublime_plugin.TextCommand):
             if not sel.empty():
                 text = self.view.substr(sel).encode(encoding)
                 hextext = binascii.hexlify(text)
-                formathex = FormatHex(hextext)
+                formathex = FormatHex(hextext.decode('utf-8'))
                 self.view.replace(edit, sel, formathex)
+
 
 class HexDecodeCommand(sublime_plugin.TextCommand):
     def run(self, edit, encoding="ascii"):
         for sel in self.view.sel():
             if not sel.empty():
                 hextext = ParseHex(self.view.substr(sel))
-                if hextext != None:
+                if hextext is not None:
                     text = binascii.unhexlify(hextext).decode(encoding)
                     cleantext = re.sub(r'[\t\n\r]', '.', text)
                     self.view.replace(edit, sel, cleantext)
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
 
+
 class FormatHexCommand(sublime_plugin.TextCommand):
     def run (self, edit, bytes):
         for sel in self.view.sel():
             if not sel.empty():
                 hextext = ParseHex(self.view.substr(sel))
-                if hextext != None:
+                if hextext is not None:
                     formathex = FormatHex(hextext, bytes)
                     self.view.replace(edit, sel, formathex)
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
 
+
 class BaseXxEncodeCommand(sublime_plugin.TextCommand):
     def run(self, edit, xx=64, table=None):
         for sel in self.view.sel():
             if not sel.empty():
-                text = self.view.substr(sel)
+                text = self.view.substr(sel).encode('utf-8')
                 bxxtext = "*No Encoding Selected*"
                 if xx == 64:
                     bxxtext = base64.b64encode(text)
                 if xx == 32:
                     bxxtext = base64.b32encode(text)
-                self.view.replace(edit, sel, bxxtext)
+                self.view.replace(edit, sel, bxxtext.decode('utf-8'))
+
 
 class BaseXxDecodeCommand(sublime_plugin.TextCommand):
     def run(self, edit, xx=64, table=None):
         for sel in self.view.sel():
             if not sel.empty():
                 bxxtext = self.view.substr(sel)
-                text = "*Improper or No Decoding Selected*"    
-                if xx == 64 and re.search('^[A-Za-z0-9+/=]+$',bxxtext):
+                text = "*Improper or No Decoding Selected*"
+                if xx == 64 and re.search('^[A-Za-z0-9+/=]+$', bxxtext):
                     if len(bxxtext) % 4 != 0:
                         bxxtext += "=" * (4 - (len(bxxtext) % 4))
-                    text = base64.b64decode(bxxtext)
-                if xx == 32 and re.search('^[A-Z2-7=]+$',bxxtext):
+                    text = base64.b64decode(bxxtext.encode('utf-8'))
+                if xx == 32 and re.search('^[A-Z2-7=]+$', bxxtext):
                     if len(bxxtext) % 8 != 0:
                         bxxtext += "=" * (8 - (len(bxxtext) % 8))
-                    text = base64.b32decode(bxxtext)
-                self.view.replace(edit, sel, text)
+                    text = base64.b32decode(bxxtext.encode('utf-8'))
+                self.view.replace(edit, sel, text.decode('utf-8'))
+
 
 class BaseXxEncodeBinaryCommand(sublime_plugin.TextCommand):
     def run(self, edit, xx=64, table=None):
@@ -219,45 +239,49 @@ class BaseXxEncodeBinaryCommand(sublime_plugin.TextCommand):
                 bxxtext = "*No Encoding Selected*"
                 if xx == 64:
                     bxxtext = binascii.b2a_base64(data)
-                self.view.replace(edit, sel, bxxtext)
+                self.view.replace(edit, sel, bxxtext.decode('utf-8'))
+
 
 class BaseXxDecodeBinaryCommand(sublime_plugin.TextCommand):
     def run(self, edit, xx=64, table=None):
         for sel in self.view.sel():
             if not sel.empty():
-            #TODO Regex for charset and check/correct padding if necessary
+                # TODO Regex for charset and check/correct padding if necessary
                 bxxtext = self.view.substr(sel)
-                text = "*No Decoding Selected*"    
+                formathex = "*No Decoding Selected*"
                 if xx == 64:
                     data = binascii.a2b_base64(bxxtext)
+                    print(data)
                     hextext = binascii.hexlify(data)
-                    formathex = FormatHex(hextext)
+                    formathex = FormatHex(hextext.decode('utf-8'))
                 self.view.replace(edit, sel, formathex)
+
 
 class TextTranslateCommand(sublime_plugin.TextCommand):
     def run(self, edit, rot, transin="AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"):
         transout = transin[rot:] + transin[:rot]
-        rottrans = maketrans(transin,transout)
+        rottrans = str.maketrans(transin, transout)
         for sel in self.view.sel():
             if not sel.empty():
                 text = self.view.substr(sel)
                 rottext = str(text).translate(rottrans)
                 self.view.replace(edit, sel, rottext)
 
+
 class HashCommand(sublime_plugin.TextCommand):
     def run(self, edit, alg=None):
         for sel in self.view.sel():
-            if not sel.empty():
-                hashtext = self.view.substr(sel)
-                if alg == "md5":
-                    hashed = hashlib.md5(hashtext).hexdigest()
-                elif alg == "sha1":
-                    hashed = hashlib.sha1(hashtext).hexdigest()
-                elif alg == "sha256":
-                    hashed = hashlib.sha256(hashtext).hexdigest()
-                else:
-                    hashed = "*No algorithm selected*"
-                self.view.replace(edit, sel, hashed)
+            hashtext = self.view.substr(sel).encode('utf-8')
+            if alg == "md5":
+                hashed = hashlib.md5(hashtext).hexdigest()
+            elif alg == "sha1":
+                hashed = hashlib.sha1(hashtext).hexdigest()
+            elif alg == "sha256":
+                hashed = hashlib.sha256(hashtext).hexdigest()
+            else:
+                hashed = "*No algorithm selected*"
+            self.view.replace(edit, sel, hashed)
+
 
 class TimestampFromIntCommand(sublime_plugin.TextCommand):
     def run(self, edit, format="Unix"):
@@ -269,11 +293,6 @@ class TimestampFromIntCommand(sublime_plugin.TextCommand):
                     timetext = time.strftime("%d-%b-%Y %H:%M:%S", time.gmtime(int(timeint)))
                 self.view.replace(edit, sel, timetext)
 
-class DisplayInputErrorCommand(sublime_plugin.TextCommand):
-    def run(self, edit, errortext="*Unknown Error*"):
-        for sel in self.view.sel():
-            if not sel.empty():
-                self.view.replace(edit, sel, errortext)
 
 class IntToHexCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -281,32 +300,35 @@ class IntToHexCommand(sublime_plugin.TextCommand):
             if not sel.empty():
                 inttext = self.view.substr(sel)
                 if re.search('^[0-9]+$', inttext):
-                    hextext = FormatHex( "%x" % (int(inttext)))
+                    hextext = FormatHex("%x" % (int(inttext)))
                     self.view.replace(edit, sel, hextext)
                 else:
                     self.view.replace(edit, sel, "*Non-integer Input*")
+
 
 class HexToIntCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for sel in self.view.sel():
             if not sel.empty():
                 hextext = ParseHex(self.view.substr(sel))
-                if hextext != None:
+                if hextext is not None:
                     text = str(int(hextext, 16))
                     self.view.replace(edit, sel, text)
                 else:
                     self.view.replace(edit, sel, "*Non-hex Input: \\xFF\\xFF xFFxFF %FF%FF \\uFFFF %uFFFF FFFF 0xFFFF expected*")
+
 
 class PeScannerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         peid_sigs = sublime.packages_path() + '/SMRT/peid.db'
         for sel in self.view.sel():
             hextext = ParseHex(self.view.substr(sel))
-            if hextext != None:
+            if hextext is not None:
                 pehex = pescanner.PEScanner(binascii.unhexlify(hextext), peid_sigs=peid_sigs)
                 report_file = self.view.window().new_file()
                 report_file.set_name("PE Scanner Report")
                 report_file.insert(edit, 0, '\n'.join(pehex.collect()))
+
 
 class GetTextRotValue(sublime_plugin.WindowCommand):
     def run(self):
@@ -320,6 +342,7 @@ class GetTextRotValue(sublime_plugin.WindowCommand):
         else:
             if self.window.active_view():
                 self.window.active_view().run_command("display_input_error", {"errortext": "*Non-integer Input*"})
+
 
 class GetSwapMap(sublime_plugin.WindowCommand):
     def run(self):
@@ -339,4 +362,3 @@ class GetSwapMap(sublime_plugin.WindowCommand):
         else:
             if self.window.active_view():
                 self.window.active_view().run_command("display_input_error", {"errortext": "*Invalid Swap Map: Use Xx:Yy format*"})
-
